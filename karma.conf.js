9a7746ea1f30257a95bd431a1935af2ea11ae61a
bomber <tangyihong063@163.com>
2019-10-08 15:57:56 +0800

    更新karma.conf.js配置，由Chrome更换为ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -55,8 +55,8 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // browsers: ['ChromeHeadless'],
-        browsers:['Chrome'],
+        browsers: ['ChromeHeadless'],
+        // browsers:['Chrome'],
         // browsers: ['IE'],
 
 
